7b70edd1472eb3f64cf3be804060df5e338d0ddd
K1NGZ3US <lukyanenkoyaroslav14@gmail.com>
2020-04-30 20:34:52 +0200

    Task2

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
 class Category(models.Model):
     """Category model"""
     name = models.CharField("Имя", max_length=100)
     slug = models.SlugField("url", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Tag(models.Model):
+    """Tag model"""
+    name = models.CharField("Тег", max_length=50)
+    slug = models.SlugField("url", max_length=50)
     
     def __str__(self):
         return self.name
     
     class Meta:
-        verbose_name = "Категория"
-        verbose_name_plural = "Категории"
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+    
+
+class Post(models.Model):
+    """Post model"""
+    title = models.CharField("Название", max_length=100)
+    mini_text = models.TextField("Поле мини-текста")
+    text = models.TextField("Поле текста")
+    created_date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField("url", max_length=100)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+
+class Comment(models.Model):
+    """Comment model"""
+    text = models.TextField("Текст комментария")
+    created_date = models.DateTimeField(auto_now_add=True)
+    moderation = models.BooleanField()
+    
+    def __str__(self):
+        return self.text
+    
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
-# Create your views here.
+from .models import Category, Post
+
+
+class HomeView(View):
+    """Home page"""
+    def get(self, request):
+        post_list = Post.objects.all()
+        return render(request, "blog/home.html", {"posts": post_list})
+
+
+class CategoryView(View):
+    """Displaying Category Articles"""
+    def get(self, request, category_name):
+        category = Category.objects.get(slug = category_name)
+        return render(request, "blog/post_list.html", {"category": category})
+    
+
+class PostView(View):
+    """Displaying Posts"""
+    def get(self, request, post_id):
+        post = Post.objects.get(slug = post_id)
+        return render(request, "blog/post.html", {"post": post})
